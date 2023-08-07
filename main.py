46967415c62a9c3520d3e5208fa059a9acd33707
Jared <j-schatzinger@onu.edu>
2023-08-07 09:50:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+"""
+Main program that reads a PLC tag and schedules a specified mission on the MiR FLEET.
+
+"""
+
+
 import fleet_commands
 from pylogix import *
 
@@ -6,12 +12,12 @@ print("Establishing connection with PLC....")
 with PLC() as comm:
     while True:
         comm.IPAddress = <ip address of PLC>
-        ret = comm.Read(<PLC Tag>)
+        ret = comm.Read(Hmi.Req[0])
         # print(ret.TagName, ret.Value, ret.Status)
         if ret.Value == 1:
             fleet_commands.pick_mission()
             print("Mission Scheduled")
-            comm.Write(<PLC Tag>, 0)  
+            comm.Write(Hmi.Req[0], 0)  
             continue
             
 
