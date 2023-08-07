ba8b5e897ea3d5c996188f5b85b30cd1d2603307
Jared Schatzinger <j-schatzinger@onu.edu>
2023-08-07 11:25:28 -0400

    First Revision

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ print("Establishing connection with PLC....")
 with PLC() as comm:
     while True:
         comm.IPAddress = '172.16.10.101'
-        ret = comm.Read("Hmi.Req[0]")
-        # print(ret.TagName, ret.Value, ret.Status)
-        if ret.Value == 1:
+        pallet_ready = comm.Read("Hmi.Req[0]")
+        robot_accept = comm.Read("Hmi.Req[1]")
+
+        if pallet_ready.Value == 1 and robot_accept.Value == 0:
             fleet_commands.pick_mission()
             print("Mission Scheduled")
-            comm.Write("Hmi.Req[0]", 0)  # PLC will write to zero after scheduling mission
+            comm.Write("Hmi.Req[1]", 1)
             continue
 
-    print("Hello")
+
 
         
+
